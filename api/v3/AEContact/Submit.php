19155895fbeca3f44525f15f671ba86770230dea
Jonas Marx <hallo@jonas-marx.com>
2019-11-12 15:36:46 +0100

    Documenting and cleaning up if/elseif/else condition

--- api/v3/AEContact/Submit.php
+++ api/v3/AEContact/Submit.php
@@ -80,7 +80,8 @@ function civicrm_api3_a_e_contact_Submit($params) {
     if (!empty($params['groups'])) {
       foreach($params['groups'] as $group_info) {
         list($group_name, $group_status) = explode(':', $group_info) + [NULL, CRM_Aeapi_Submission::GROUP_STATUS_ADDED];
-        // Specific case of to petition for which the "Newsletter" group is enabled.
+        /* This group is the main newsletter for the petition. If the contact accepted the reception
+            of a newsletter, we send a Double OptIn by the MailingEventSubscribe event */
         if (strcasecmp($group_status, CRM_Aeapi_Submission::NEWSLETTER_GROUP) === 0) {
           if ($params['want_newsletter']) {
             // Set group-subscription status to pending
@@ -98,7 +99,9 @@ function civicrm_api3_a_e_contact_Submit($params) {
           }
           continue;
         }
-        if (strcasecmp($group_status, CRM_Aeapi_Submission::GROUP_STATUS_PENDING) === 0) {
+        /* Pending groups also need a Double-OptIn, but it doesn't depend
+            on accepting the newsletter */
+        elseif (strcasecmp($group_status, CRM_Aeapi_Submission::GROUP_STATUS_PENDING) === 0) {
           $mailing_event_subscribe = civicrm_api3('MailingEventSubscribe', 'create', array(
             'contact_id' => $contact_id,
             'email' => $contact['email'],
@@ -110,8 +113,11 @@ function civicrm_api3_a_e_contact_Submit($params) {
             'subject' => 'Requested: '.$group_name.' (DoubleOptIn sent)',
             'status_id' => 'Completed'
           ));
+          continue;
         }
-        if (strcasecmp($group_status, CRM_Aeapi_Submission::DOI_NEW_GROUP) === 0) {
+        /* For some Welcome Journeys, we only want new contacts to join
+            and only in case they just accepted the newsletter */
+        elseif (strcasecmp($group_status, CRM_Aeapi_Submission::DOI_NEW_GROUP) === 0) {
           if ($params['want_newsletter'] && $contact_is_new) {
             $group_contact = civicrm_api3('GroupContact', 'create', array(
               'contact_id' => $contact_id,
@@ -119,7 +125,9 @@ function civicrm_api3_a_e_contact_Submit($params) {
               'status' => 'Added'
             ));
           }
+          continue;
         }
+        /* All other groups (including the status "Added") will just get added to the contact */
         else {
           $group_contact = civicrm_api3('GroupContact', 'create', array(
             'contact_id' => $contact_id,
